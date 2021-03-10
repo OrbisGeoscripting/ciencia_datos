3975e6889022829b6c2620df5b8fe0e05cf95d4c
Ever Salazar <andressalmercado@gmail.com>
2021-03-10 16:54:45 -0500

    Juegos para la clase

---
+++ Juego del ahorcado.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 13:02:40 2020
+
+@author: ASUS
+"""
+import random
+
+nombre=input("Hola ¿cómo estás?, vamos a jugar al Ahorcado \npero primero, me gustaría saber tu nombre ¿Cómo te llamas?: ")
+print("\nBienvenido, "+nombre,"\nEs hora de jugar")
+print(" ")
+print("\n¡Comienza a adivinar!")
+
+with open(r'C:\Users\ASUS\Documents\Python\Diplomado\Git - Github\Ejercicio remotos\palabras-master', 'r', encoding="utf8") as f:
+    palabra = [line.strip() for line in f]
+palabras = random.choice(palabra)
+tupalabra=" "
+vidas= 5
+
+while vidas > 0:
+    fallas=0
+    
+    
+    for letra in palabras:
+        if letra in tupalabra:
+            print(letra,end=" ")
+        else:
+            print("_",end=" ")
+            fallas+=1
+            
+    if fallas==0:
+        print("")
+        print("¡¡¡Felicidades!!! \nGanaste :DDDD")
+        break
+    
+    tuletra=input("Introduce tu letra: ")
+
+    tuletra = tuletra.lower()
+    tupalabra+=tuletra
+    
+
+    if tuletra not in palabras:
+        vidas-=1
+        print("¡Equivocacion\n!")
+        print("Te quedan ",+vidas," vidas")
+    if vidas== 0:
+        print(f"¡Perdiste!, la palabra era: {palabras}")
+else:
+    print("\ngracias por participar")
